11563dea1330b792b672302316bac7f331190a64
Karan Jeet <karanjeet96@gmail.com>
2015-11-07 01:00:32 +0530

    passport fb module

---
+++ config/auth.js
@@ -0,0 +1,13 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '779525218836331',
+		'clientSecret': '22ea07cd7644eb3e6eac1ffd15ddbde8',
+		'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+	},
+
+	'googleAuth' : {
+		'clientID': 'enter client id here',
+		'clientSecret': 'enter client secret here',
+		'callbackURL': 'enter callback here'
+	}
+}
